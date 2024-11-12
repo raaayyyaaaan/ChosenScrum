df5649c6f805ba323309dc44cd751a47d3b50513
1002215 <149686629+1002215@users.noreply.github.com>
2024-11-11 21:47:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+# The flask module allows us to create a flask framework, which we can use to send requests using http requests.
+# The time module allows us to fix the duration of time each function moves by seconds.
+# The PCA9685 module allows us to control how much electrical power needs to be sent
+# The Login module returns the values showing whether the account was found in the database, whether the password matches, and whether the user has access to the Chosen network.
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 from Login import *
 from PCA9685 import PCA9685
+import requests
 import logging
 from datetime import datetime
-import requests
 
 # Direct to where the electrical power needs to be sent and how much needs to be sent
 pwm = PCA9685(0x40, debug=False)
@@ -11,12 +15,10 @@ pwm.setPWMFreq(20)
 
 # Create the flask framework
 app = Flask(__name__, template_folder='templates')
-app.secret_key = "1234"
 
 # Global list to store logs for display on the webpage
 log_messages = []
 
-# Helper function to log messages to console and the log list
 def log_action(message):
     logging.info(message)
     log_messages.append(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {message}")
@@ -24,50 +26,69 @@ def log_action(message):
     if len(log_messages) > 100:
         log_messages.pop(0)
 
-# Endpoint to retrieve logs as JSON
+        # Endpoint to retrieve logs as JSON
 @app.route('/logs', methods=['GET'])
 def get_logs():
     return jsonify(log_messages)
 
 def send_request(url):  # Sends requests to the host url
    x = requests.post(url)
-def move_fwd(self, speed):
+   return x
+
+# The TankRobot class allows us to connect to the motors and control the amount of electrical power being sent to each of the motors. It also has the functions to get the tank to move forward, backward, left, right, and stop.
+class TankRobot:
+    def __init__(self):
+        self.PWMA = 0 # Controls the power supplied and connects it to hardware
+        self.AIN1 = 1 # Controls direction of the first motor
+        self.AIN2 = 2 # Controls the opposite direction of the first motor
+        self.PWMB = 5 # Controls the power supplied and connects it to hardware
+        self.BIN1 = 3 # Controls direction of the second motor
+        self.BIN2 = 4 # Controls the opposite direction of the second motor
+
+# Gets the tank to move forward for three seconds before stopping, taking in speed as a parameter
+    def move_fwd(self, speed):
+        pwm.setDutycycle(self.PWMA, speed)
+        pwm.setLevel(self.AIN1, 0)
+        pwm.setLevel(self.AIN2, 1)
         pwm.setDutycycle(self.PWMB, speed)
         pwm.setLevel(self.BIN1, 1)
         pwm.setLevel(self.BIN2, 0)
         log_action("Rover moved forward")
-
 # Gets the tank to move backward for three seconds before stopping, taking in speed as a parameter.
     def move_backward(self, speed):
+        pwm.setDutycycle(self.PWMA, speed)
+        pwm.setLevel(self.AIN1, 1) # Left motor goes backwards
+        pwm.setLevel(self.AIN2, 0)
         pwm.setDutycycle(self.PWMB, speed)
         pwm.setLevel(self.BIN1, 0) #Right motor goes forwards
         pwm.setLevel(self.BIN2, 1)
-
         log_action("Rover moved backward")
+
 # Gets the tank to turn left for three seconds before stopping, taking in speed as a parameter.
     def turn_left(self, speed):
         pwm.setDutycycle(self.PWMA, speed)
-    def turn_left(self, speed):
+        pwm.setLevel(self.AIN1, 1)
+        pwm.setLevel(self.AIN2, 0)   # Left motor goes backward
         pwm.setDutycycle(self.PWMB, speed)
         pwm.setLevel(self.BIN1, 1)
         pwm.setLevel(self.BIN2, 0)  # Right motor goes forward
-
         log_action("Rover turned left")
+
 # Gets the tank to turn right for three seconds before stopping, taking in speed as a parameter.
     def turn_right(self, speed):
         pwm.setDutycycle(self.PWMA, speed) # Left motor goes forward
-    def turn_right(self, speed):
+        pwm.setLevel(self.AIN1, 0)
+        pwm.setLevel(self.AIN2, 1)
         pwm.setDutycycle(self.PWMB, speed) # Right motor goes backward
         pwm.setLevel(self.BIN1, 0)
         pwm.setLevel(self.BIN2, 1)
-
         log_action("Rover turned right")
 
 # Gets the tank to stop, taking in no parameters
     def stop(self):
         pwm.setDutycycle(self.PWMA, 0)  # Stop left motor
         pwm.setDutycycle(self.PWMB, 0)  # Stop right motor
-
+        log_action("Rover stopped")
 tank_robot = TankRobot()
 
 # We created a POST route with no parameters, then ran the move_fwd command on the tank_robot. Then, we returned the JSON dictionary storing the confirmation that the command was run.
@@ -100,19 +121,30 @@ def stop():
     tank_robot.stop() # Stop
     return jsonify({'command': 'STOP'}) # Return confirmation that the function was ran
 
-
+# This opens up the website at the login page
 @app.route('/')
 def start():
   log_action("Website opened at login page")
   return redirect(url_for('login'))
 
 # Using values from the Login module, it determines whether the user exists in the database, whether the information is valid, and whether the user gets access to the Chosen Network. If they do, they get redirected to the main page.
-@@ -122,18 +144,23 @@ def login():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':  # If the form is submitted via POSt
+     send = Login()
+     # Retrieve the form data using 'request.form' which is a dictionary-like object
+     username = request.form['username']
+     password = request.form['password']
+     action = request.form.get('action')
+     if action == 'login':
+          send.sendinfo(username, password)
+          user_exists = send.bFoundAccount
+          user_valid = send.LoginSuccessful
           # You can now use this data for further processing (like checking credentials)
           if user_exists == True:
               if user_valid == True:
                   log_action(f"User {username} logged in successfully")
-                  return redirect(url_for('buttons'))
+                  return redirect(url_for('screen'))
               else:
                   log_action(f"Login failed for user {username}: Incorrect password")
                   return 'Password is incorrect, please try again.'
@@ -131,35 +163,34 @@ def start():
              return "Account created!"
   return render_template('login.html') # Use login html file for the aesthetics
 
-@@ -158,6 +185,9 @@ def buttons():
-	if request.method == 'POST':
+# This function takes in JSON POST requests from the buttons, and makes the robot move accordingly by sending requests to the robot. 
+@app.route('/buttons', methods=['GET', 'POST'])
+def buttons():
+   if request.method == 'POST':
        # Get JSON data from the buttons
        jsondata = request.get_json()
        action = jsondata.get('action')
        if action == 'fwd':
-           send_request('http://192.168.1.25:5000/fwd')
+           send_request('http://127.0.0.1:5000/fwd')
        if action == 'bwd':
-           send_request('http://192.168.1.25:5000/bwd')
+           send_request('http://127.0.0.1:5000/bwd')
        if action == 'right':
-           send_request('http://192.168.1.25:5000/right')
+           send_request('http://127.0.0.1:5000/right')
        if action == 'left':
-           send_request('http://192.168.1.25:5000/left')
+           send_request('http://127.0.0.1:5000/left')
        if action == 'stop':
-           send_request('http://192.168.1.25:5000/stop')
+           send_request('http://127.0.0.1:5000/stop')
         
-
    return render_template('buttons.html') # Use the buttons html file for the aesthetics
-
+   
 # Show the console log front end
-@app.route('/console', methods = ['GET', 'POST'])
+@app.route('/logs', methods = ['GET', 'POST'])
 def console():
-    return render_template('console.html')
-
+    return render_template('logs.html')
 #Show the screen separating the sebite into four parts: one with the buttons, one with the console log, and the rest as placeholders.
 @app.route('/screen/', methods = ['GET', 'POST'])
 def screen():
     return render_template('screen.html')
 
-
 if __name__ == "__main__": # This runs the app
-    app.run(host='0.0.0.0', debug=True, port=5000, use_reloader=False) # Where the API will be hosted
+    app.run(host='127.0.0.1', debug=True, port=5000, use_reloader=False) # Where the API will be host
